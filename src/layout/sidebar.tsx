cf236e2c009e763369a88dd89bede5d77d55c48b
Rajnish Singh <rajnish42413@gmail.com>
2021-05-05 13:02:00 +0530

    added selected active menu

--- src/layout/sidebar.tsx
+++ src/layout/sidebar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Menu, Layout, Drawer, Button } from 'antd';
 import {
   HomeOutlined,
@@ -7,20 +7,31 @@ import {
   PayCircleFilled,
   MenuOutlined
 } from '@ant-design/icons';
-import { Link } from 'react-router-dom';
+import { Link, useRouteMatch, useLocation } from 'react-router-dom';
 
 export default function AppSidebar() {
   const [visible, setVisible] = useState(false);
+  const location = useLocation();
+  let intialSelected = ['2'];
+  if (location.pathname === '/players') intialSelected = ['2'];
+  if (location.pathname === '/new-register') intialSelected = ['3'];
+  if (location.pathname === '/pay') intialSelected = ['4'];
+  const [selectedMenu, setSelectedMenu] = useState(intialSelected);
+
+  const onSelect = ({ key }: any) => {
+    setSelectedMenu([key]);
+  };
+
   return (
     <>
       <div className="prs-sidebar">
         <Layout.Sider style={{ paddingTop: 30 }}>
           <div className="logo" />
-          {menu('dark')}
+          {menu('dark', selectedMenu, onSelect)}
         </Layout.Sider>
       </div>
       <Drawer title="PRS-AICF" placement="left" onClose={() => setVisible(false)} visible={visible}>
-        {menu('light')}
+        {menu('light', selectedMenu, onSelect)}
       </Drawer>
       <Button type="primary" onClick={() => setVisible(!visible)} className="mobile-navabar-btn">
         <MenuOutlined />
@@ -29,28 +40,36 @@ export default function AppSidebar() {
   );
 }
 
-const menu = (theme: 'dark' | 'light') => (
-  <Menu theme={theme} mode="inline" defaultSelectedKeys={['2']} className="prs-mobile-menu">
-    <Menu.Item key="1" icon={<HomeOutlined />}>
-      <a href="https://aicf.in/" target="_blank" rel="noopener noreferrer">
-        AICF Home
-      </a>
-    </Menu.Item>
-    <Menu.Item key="2" icon={<SearchOutlined />}>
-      <Link to="/players">Search Player</Link>
-    </Menu.Item>
-    <Menu.Item key="3" icon={<FileTextOutlined />}>
-      <Link to="/new-register">Register New</Link>
-    </Menu.Item>
-    <Menu.Item key="4" icon={<PayCircleFilled />}>
-      <Link to="/pay">Other Payment</Link>
-    </Menu.Item>
-    {/* <Menu.Item key="4" icon={<DollarCircleOutlined />}>
+const menu = (theme: 'dark' | 'light', selectedKey: Array<string>, onSelect: any) => {
+  return (
+    <Menu
+      theme={theme}
+      mode="inline"
+      defaultSelectedKeys={selectedKey}
+      className="prs-mobile-menu"
+      onSelect={onSelect}
+    >
+      <Menu.Item key="1" icon={<HomeOutlined />}>
+        <a href="https://aicf.in/" target="_blank" rel="noopener noreferrer">
+          AICF Home
+        </a>
+      </Menu.Item>
+      <Menu.Item key="2" icon={<SearchOutlined />}>
+        <Link to="/players">Search Player</Link>
+      </Menu.Item>
+      <Menu.Item key="3" icon={<FileTextOutlined />}>
+        <Link to="/new-register">Register New</Link>
+      </Menu.Item>
+      <Menu.Item key="4" icon={<PayCircleFilled />}>
+        <Link to="/pay">Donate</Link>
+      </Menu.Item>
+      {/* <Menu.Item key="4" icon={<DollarCircleOutlined />}>
           Renew Membership
         </Menu.Item> */}
 
-    {/* <Menu.Item key="5" icon={<DollarCircleOutlined />}>
+      {/* <Menu.Item key="5" icon={<DollarCircleOutlined />}>
           <Link to="/national-tournament-registration">National Tournament Registration</Link>
         </Menu.Item> */}
-  </Menu>
-);
+    </Menu>
+  );
+};
