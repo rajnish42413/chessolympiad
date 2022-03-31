80a374026a2e5c43252af6da5febdb58a4b03fec
rajnish42413 <rajnish42413@gmail.com>
2022-03-31 15:25:27 +0530

    Added live api

--- src/constants/general.tsx
+++ src/constants/general.tsx
@@ -1,5 +1,5 @@
 // export const API_URL = 'https://admin.aicf.in/api/';
-export const API_URL = 'http://http://65.0.86.200/api/';
+export const API_URL = 'http://65.0.86.200/api/';
 export const RZ_KEY = 'rzp_live_WRK0rd8dW3aZ3g';
 export const RZ_SECRET = 'sM4DVP4uYY05Hn1RXPBu6UxZ';
 export const MAX_PLAYER_ALLOW = 8;
